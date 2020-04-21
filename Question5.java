8e543342854645c2a55125081dfa507b1caac845
tlsloth <64000990+tlsloth@users.noreply.github.com>
2020-04-21 10:03:24 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,32 +1,45 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Question5
 {
   public static void main(String[] args)
   {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
-    Scanner in = new Scanner(System.in);
+    System.out.print("Enter the number of numbers: ");
+    int numofnums = in.nextInt();
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
+    while(numbers.size() != numofnums){
+        String reply = String.format("Number #%d: ",numbers.size()+1);
+        System.out.print(reply);
+        Integer integer = in.nextInt();
+        numbers.add(integer);
+    }
+    Integer[] numarray = numbers.toArray(new Integer[0]);
+    HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();
+    int max = 1;
+    int largestnum = 0;
+    for (int i = 0; i < numbers.size(); i++){
+        //check if number is in hashmap yet else add it
+        if (hm.get(numarray[i]) != null){
+            //get the value for the key and increase by one
+            int count = hm.get(numarray[i]);
+            count++;
+            hm.put(numarray[i],count);
+            //find largest count and number and set updated value
+            if (count > max){
+                max = count;
+                largestnum = numarray[i];
+            }
+        }
+
+        else{
+            hm.put(numarray[i],1);
+        }
+
+        //else put into hashmap
+    }
+    String reply = String.format("Mode of the list: %d",largestnum);
+    System.out.print(reply);
+
     
   }
 }
